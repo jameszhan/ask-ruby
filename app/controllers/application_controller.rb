0a9dc5a679cf4641b0c4d1f2f577013549c99b75
James Zhan <zhiqiangzhan@gmail.com>
2013-03-11 10:09:43 +0800

    Complete the rails erb template for our project.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to questions_path, :alert => t("common.access_denied")
   end  
+  
+  
+  def current_tags
+    @current_tags ||=  if params[:tags].kind_of?(String)
+      params[:tags].split("+")
+    elsif params[:tags].kind_of?(Array)
+      params[:tags]
+    else
+      []
+    end
+  end
+  
+  
+  helper_method :current_tags
 end
