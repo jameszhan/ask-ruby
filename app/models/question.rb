979d2d284f8d19d66e364779b081019a9876e175
mulixiang199016 <mulixiang199016@gmail.com>
2013-03-11 08:38:34 +0800

    manual merge 2

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,13 +15,11 @@ class Question
 
   belongs_to :user
 
-  def viewed!(user)
-    if user && user != self.user
-      view_count_id = "#{self.id}-#{user.id}"
-      if ViewsCount.where({ identity: view_count_id }).first.nil?
-        ViewsCount.create(identity: view_count_id)
-        self.inc(:views_count, 1)
-      end
-    end
+  def viewed!(ip)
+    view_count_id = "#{self.id}-#{ip}"
+    if ViewsCount.where({ identity: view_count_id }).first.nil?
+      ViewsCount.create(identity: view_count_id)
+      self.inc(:views_count, 1)
+    end    
   end
 end
