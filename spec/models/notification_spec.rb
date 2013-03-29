9730187e9b3caf7580c115490afd6c5c6b163ec3
mulixiang199016 <mulixiang199016@gmail.com>
2013-03-29 16:55:48 +0800

    modify rspec

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Notification do
 	it { should respond_to(:read) }
-	it { should respond_to(:source) }
-	it { should validate_presence_of(:source) }
+	it { should respond_to(:source_type) }
+	it { should validate_presence_of(:source_type) }
 	it { should respond_to(:source_id) }
 	it { should validate_presence_of(:source_id) }
-	it { should respond_to(:user_id) }
-	it { should validate_presence_of(:user) }
+	it { should respond_to(:user) }
+	it { should validate_presence_of(:user_id) }
 end
