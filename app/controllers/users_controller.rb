76540324cfcbddad9e67697059be3997671a4451
mulixiang199016 <mulixiang199016@gmail.com>
2013-04-07 09:21:05 +0800

    modify user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < Devise::RegistrationsController
+  before_filter :find_user
+  
   def show
-    @user = User.find(params[:id])
   end
+
+  def follow
+    if current_user.follow?(@user)
+      current_user.unfollow!(@user)
+    else
+      current_user.follow!(@user)
+    end
+    respond_to do |format|
+      format.html { redirect_to user_path(@user) }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def find_user
+      @user = User.find(params[:id])
+    end
 end
