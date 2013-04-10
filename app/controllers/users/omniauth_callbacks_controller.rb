0fe1db4d444975741bb67d5d80f9eba804e713c1
James Zhan <zhiqiangzhan@gmail.com>
2013-04-10 11:19:08 +0800

    Fix the small bugs.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,10 +15,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def login_with_omniauth
       user = User.from_omniauth(request.env["omniauth.auth"])
       if user.persisted?
-        session[:login_type] = "omniauth"
-        flash.notice = "Signed in!"
-        sign_in_and_redirect user
-      elsif user.email && User.where(email: user.email)
+        login_success
+      elsif user.email && User.where(email: user.email).first
         redirect_to new_user_session_path, alert: "用户 #{user.email} 已经存在，你可以先用该账号登陆，然后绑定#{env["omniauth.auth"][:provider]} 帐号。"
       else
         session["devise.user_attributes"] = user.attributes
@@ -29,6 +27,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def bind_omniauth_account
       current_user.bind_omniauth(env["omniauth.auth"]) #Add an auth to existing
       redirect_to edit_user_registration_path, notice: "成功绑定了 #{env["omniauth.auth"][:provider]} 帐号。"
+    end
+    
+    def login_success
+      session[:login_type] = "omniauth"
+      flash.notice = "Signed in!"
+      sign_in_and_redirect user
     end
       
 end
