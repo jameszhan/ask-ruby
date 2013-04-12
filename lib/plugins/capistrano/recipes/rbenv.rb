30f4986008a32ac5a48a91c7f497bc28dccd8e33
James Zhan <zhiqiangzhan@gmail.com>
2013-04-12 09:45:42 +0800

    add utf8 encoding to app/controllers/users/omniauth_callbacks_controller.rb.

--- lib/plugins/capistrano/recipes/rbenv.rb
+++ lib/plugins/capistrano/recipes/rbenv.rb
@@ -5,7 +5,8 @@ namespace :rbenv do
   desc "Install rbenv, Ruby, and the Bundler gem"
   task :install, roles: :app do
     run "#{sudo} apt-get -y install curl git-core"
-    run "#{sudo} apt-get -y install imagemagick --fix-missing"
+    #Install rmagick
+    run "#{sudo} apt-get -y install libdjvulibre-dev libjpeg-dev libtiff-dev libwmf-dev libmagickcore-dev libmagickwand-dev libmagick++-dev"
     run "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
     bashrc = <<-BASHRC
 if [ -d $HOME/.rbenv ]; then 
