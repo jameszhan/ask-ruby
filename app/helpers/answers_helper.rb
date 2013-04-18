c2acf6b638c7361c95ac859cdfccec447d473930
mulixiang199016 <mulixiang199016@gmail.com>
2013-04-18 18:10:46 +0800

    marker soluation answer

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,11 @@
 module AnswersHelper
+  def sloved(question,answers)
+    answers.each_index do |i|
+      if question.answered_with == answers[i]
+        @index = i
+      end
+    end
+    sloved_answer = answers.delete_at(@index)
+    answers.unshift(sloved_answer)
+  end
 end
