92b8e24ee0cfe496d399b0dc651f2527646d4e14
mulixiang199016 <mulixiang199016@gmail.com>
2013-04-19 02:14:29 +0800

    refactor show markered answer

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,11 +1,2 @@
 module AnswersHelper
-  def sloved(question,answers)
-    answers.each_index do |i|
-      if question.answered_with == answers[i]
-        @index = i
-      end
-    end
-    sloved_answer = answers.delete_at(@index)
-    answers.unshift(sloved_answer)
-  end
 end
