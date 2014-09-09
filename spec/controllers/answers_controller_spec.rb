5e6d87c8eb0f503ed02d987cbf9257a5fcfa707d
James Zhan <zhiqiangzhan@gmail.com>
2014-09-09 13:46:42 +0800

    add more specs.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -34,7 +34,7 @@ describe AnswersController do
   end
 
   # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
+  # in order to pass any filters.js (e.g. authentication) defined in
   # AnswersController. Be sure to keep this updated too.
   def valid_session
     {}
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -33,7 +33,7 @@ describe TagsController do
   end
 
   # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
+  # in order to pass any filters.js (e.g. authentication) defined in
   # TagsController. Be sure to keep this updated too.
   def valid_session
     {}
